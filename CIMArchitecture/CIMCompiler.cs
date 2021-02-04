46f9b4c8747862d4bd602f1a58a0124fbeb09cab
Mclemore, Ciante (US) <ciante.mclemore@lmco.com>
2021-02-04 18:01:07 -0500

    Updated compiler to invoke the instruction given by user

--- CIMArchitecture/CIMCompiler.cs
+++ CIMArchitecture/CIMCompiler.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
+using System.Reflection;
 
 namespace CIMArchitecture
 {
@@ -10,31 +11,54 @@ namespace CIMArchitecture
     /// </summary>
     class CIMCompiler
     {
-        private List<Instruction> _availableInstructions;
-        private List<string> _commands;
+        private Dictionary<string, Instruction> _availableInstructions;
+        private Dictionary<string, Register> _availableRegisters;
+        private List<string> _userInput;
 
-        public CIMCompiler(List<Instruction> instructions, List<string> commands) 
+        public CIMCompiler(Dictionary<string, Instruction> instructions, Dictionary<string, Register> registers, List<string> commands) 
         {
-            _commands = commands;
+            _userInput = commands;
             _availableInstructions = instructions;
-            ReadCommands(_commands);
+            ReadCommands(_userInput);
         }
 
-        public void ReadCommands(List<string> commands) 
+        public void ReadCommands(List<string> userInput)
         {
-            var instructions = SplitCommands(commands);
+            var commands = SplitCommands(userInput);
 
-            foreach(var instr in instructions) 
+            ProcessCommands(commands);
+        }
+
+        private void ProcessCommands(List<List<string>> commands)
+        {
+            foreach (var command in commands)
             {
-                var count = 1;
-                Console.WriteLine($"Instruction {count}");
-                foreach (var cmd in instr) 
-                {
-                    Console.WriteLine($"\t{cmd}");    
-                }
+                ProcessCommand(command);
+            }
+        }
+
+        private void ProcessCommand(List<string> command) 
+        {
+            //Get instruction for method call
+            var instruction = _availableInstructions[command[0]];
+
+            //Store the rest as parameters for the function call
+            var parameters = new List<string>();
+
+            //Get remaining parameters used
+            int j = 0;
+            for (int i = 1; i < command.Count; i++) 
+            {
+                parameters[j] = command[i];
+                j++;
             }
 
-            
+            MethodInfo method = typeof(CIMCompiler).GetMethod(instruction.FunctionName);
+
+            if (method != null) 
+            {
+                method.Invoke(this, new object[] { parameters });
+            }
         }
 
         private List<List<string>> SplitCommands(List<string> commands) 
@@ -47,5 +71,27 @@ namespace CIMArchitecture
             }
             return parsedCommands;
         }
+
+        
+        private void LoadImmediate(string destination, string value) 
+        {
+            //Get destination register
+            var destReg = _availableRegisters[destination];
+
+            int numValue;
+            var IsNumber = Int32.TryParse(value, out numValue);
+
+            if (numValue > 65536) 
+            {
+                //throw error
+            }
+        }
+
+        private void AddTwoNumbers(Register destination, int first, int second) 
+        {
+        
+        }
+
+
     }
 }

81a4977d16387055fd241b94fc88ffb3925c9c1b
Tay D'Von <ciantemclemore@gmail.com>
2021-02-03 20:04:21 -0500

    Initial instruction model

---
+++ CIMArchitecture/Models/Instruction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CIMArchitecture
+{
+    /// <summary>
+    /// Defines the structure for an instruction
+    /// </summary>
+    class Instruction
+    {
+        public string Name { get; set; }
+
+        public int Value { get; set; }
+
+        public string FunctionName { get; set; }
+    }
+}

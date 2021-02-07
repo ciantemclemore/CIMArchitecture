ef47b189cb779b0cdf9de7a44f585ae58c20100c
Tay D'Von <ciantemclemore@gmail.com>
2021-02-07 11:22:57 -0500

    Add equality checking to instruction model

--- CIMArchitecture/Models/Instruction.cs
+++ CIMArchitecture/Models/Instruction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 namespace CIMArchitecture
@@ -7,12 +8,32 @@ namespace CIMArchitecture
     /// <summary>
     /// Defines the structure for an instruction
     /// </summary>
-    class Instruction
+    class Instruction : IEquatable<Instruction>
     {
         public string Name { get; set; }
 
         public string Value { get; set; }
 
         public string FunctionName { get; set; }
+
+        public bool Equals([AllowNull] Instruction other)
+        {
+            if (other == null)
+                return false;
+            return this.Name.Equals(other.Name);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            Instruction objAsPart = obj as Instruction;
+            if (objAsPart == null) return false;
+            else return Equals(objAsPart);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
     }
 }

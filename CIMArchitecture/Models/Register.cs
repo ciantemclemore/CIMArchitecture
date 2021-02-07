8cba626ad6dec6031f5f941a815104b9eb6c64ba
Tay D'Von <ciantemclemore@gmail.com>
2021-02-07 11:23:12 -0500

    Added equality checking to register model

--- CIMArchitecture/Models/Register.cs
+++ CIMArchitecture/Models/Register.cs
@@ -1,18 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Diagnostics.CodeAnalysis;
 
 namespace CIMArchitecture
 {
     /// <summary>
     /// Defines the structure for a register.
     /// </summary>
-    class Register
+    class Register: IEquatable<Register>
     {
         public string Name { get; set; }
 
         public int DataValue { get; set; }
 
         public string BitValue { get; set; }
+
+        public bool Equals([AllowNull] Register other)
+        {
+            if (other == null)
+                return false;
+            return this.Name.Equals(other.Name);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            Register objAsPart = obj as Register;
+            if (objAsPart == null) return false;
+            else return Equals(objAsPart);
+        }
+
+        public override int GetHashCode()
+        {
+            return DataValue;
+        }
     }
 }

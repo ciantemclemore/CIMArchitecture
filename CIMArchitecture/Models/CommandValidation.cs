8eb47fb8b64d2ec896044ddf7f218016a3b0277b
Mclemore, Ciante (US) <ciante.mclemore@lmco.com>
2021-02-10 12:15:37 -0500

    Added a command validation class to return useful information to users when entering invalid commands.

---
+++ CIMArchitecture/Models/CommandValidation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CIMArchitecture.Models
+{
+    class CommandValidation
+    {
+        public bool IsValid { get; set; } = true;
+
+        public string Message { get; set; }
+    }
+}

ee4bc7ba0291620ef9d1a40b33439db49711399d
Mclemore, Ciante (US) <ciante.mclemore@lmco.com>
2021-02-10 12:16:21 -0500

    The result model will now be returned instead of a dictionary of keys and values. The result now contains all the necessary information for printing.

---
+++ CIMArchitecture/Models/Result.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CIMArchitecture.Models
+{
+    public class Result
+    {
+        public Instruction Instruction { get; set; }
+
+        public string BinaryString { get; set; }
+
+        public bool IsError { get; set; }
+
+        public string ErrorMessage => IsError ? "Value Not Supported" : "None";
+
+    }
+}
